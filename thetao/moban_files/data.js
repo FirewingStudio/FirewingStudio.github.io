28f36840ea3c0b6e6c54df3ca9abf74f4cfa8b70
FirewingStudio <firewings.venture@gmail.com>
2024-11-24 18:15:59 +0800

    home and thetao

---
+++ thetao/moban_files/data.js
@@ -0,0 +1,41 @@
+var d_share = [
+    {
+        "type": "href",
+        "imgSrc": "",
+        "href": "https://www.instagram.com/firewings.venture/",
+        "modalImgSrc": ""
+    },
+    {
+        "type": "href",
+        "imgSrc": "",
+        "href": "https://www.facebook.com/profile.php?id=61566609436483",
+        "modalImgSrc": ""
+    },
+    {
+        "type": "href",
+        "imgSrc": "",
+        "href": "https://x.com/firewings_r",
+        "modalImgSrc": ""
+    },
+    {
+        "type": "href",
+        "imgSrc": "",
+        "href": "https://www.youtube.com/channel/UCerhOZpO8JezITB7ZZ2JcjA",
+        "modalImgSrc": ""
+    }
+];
+
+var d_download = [
+    {
+        "type": "download",
+        "href": "https://apps.apple.com/us/app/google-maps/id585027354",
+        "imgSrc": "",
+        "modalImgSrc": ""
+    },
+    {
+        "type": "download",
+        "href": "https://play.google.com/store/apps/details?id=com.google.android.apps.maps",
+        "imgSrc": "",
+        "modalImgSrc": ""
+    }
+];
